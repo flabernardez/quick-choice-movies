4bcf286146b71aebdfc01da41317372013f63d0b
flabernardez <flaviabernardez@gmail.com>
2025-11-08 09:45:58 +0100

    init commit

---
+++ quick-choice-movies.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Plugin Name:       Quick Choice Movies
+ * Description:       A quick choice movies game
+ * Requires at least: 6.6
+ * Requires PHP:      7.0
+ * Version:           0.1.0
+ * Author:            Flavia Bernárdez Rodríguez
+ * License:           GPL-2.0-or-later
+ * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
+ * Text Domain:       quick-choice-movies
+ *
+ * @package CreateBlock
+ */
+
+if ( ! defined( 'ABSPATH' ) ) {
+    exit; // Exit if accessed directly.
+}
